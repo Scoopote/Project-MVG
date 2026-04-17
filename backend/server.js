8009320025ecf28cc960c82adafdb0ebf4463403
Scoopote <Louis.Coulongeon2106@gmail.com>
2026-04-17 02:38:30 +0200

    feat: update server response message in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Voilà la réponse du serveur !');
+  res.end('Eh bah alors, on marche ou bien ?');
 });
 
 server.listen(process.env.PORT || 3000);
