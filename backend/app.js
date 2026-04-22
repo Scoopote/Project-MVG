fe1d6b79c733f142e9a9030d709a1f716e8ea9a4
Scoopote <Louis.Coulongeon2106@gmail.com>
2026-04-22 23:58:42 +0200

    feat: integrate mongoose for MongoDB connection and add Thing model

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+async function connectMongo() {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://Scoopote:TrolilolDu31@cluster0.xytjy5r.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0',
+      {
+        serverSelectionTimeoutMS: 5000
+      }
+    );
+    console.log('Connexion à MongoDB réussie !');
+  } catch (error) {
+    console.log('Connexion à MongoDB échouée !');
+    console.log(error);
+  }
+}
+
+connectMongo();
+
+app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -15,13 +35,14 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api/stuff', (req, res, next) => {
+app.get('/api/stuff', (req, res, next) => {
   const stuff = [
     {
       _id: 'oeihfzeoi',
       title: 'Mon premier objet',
       description: 'Les infos de mon premier objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      imageUrl:
+        'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
       price: 4900,
       userId: 'qsomihvqios',
     },
@@ -29,13 +50,19 @@ app.use('/api/stuff', (req, res, next) => {
       _id: 'oeihfzeomoihi',
       title: 'Mon deuxième objet',
       description: 'Les infos de mon deuxième objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      imageUrl:
+        'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
       price: 2900,
       userId: 'qsomihvqios',
     },
   ];
 
   res.status(200).json(stuff);
+});
+
+app.post('/api/stuff', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({ message: 'Objet créé !' });
 });
 
 module.exports = app;
