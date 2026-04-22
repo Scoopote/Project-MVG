ac7e8cbd1f7b4bf5f52fd6753e05f1a51fdd6abf
Scoopote <Louis.Coulongeon2106@gmail.com>
2026-04-23 00:14:43 +0200

    feat: implement CRUD operations for Thing model and set up routes

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,8 @@
+const Thing = require('./models/thing');
 const express = require('express');
 const mongoose = require('mongoose');
+
+const stuffRoutes = require('./routes/stuff');
 
 const app = express();
 
@@ -35,34 +38,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/api/stuff', (req, res, next) => {
-  const stuff = [
-    {
-      _id: 'oeihfzeoi',
-      title: 'Mon premier objet',
-      description: 'Les infos de mon premier objet',
-      imageUrl:
-        'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 4900,
-      userId: 'qsomihvqios',
-    },
-    {
-      _id: 'oeihfzeomoihi',
-      title: 'Mon deuxième objet',
-      description: 'Les infos de mon deuxième objet',
-      imageUrl:
-        'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 2900,
-      userId: 'qsomihvqios',
-    },
-  ];
-
-  res.status(200).json(stuff);
-});
-
-app.post('/api/stuff', (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({ message: 'Objet créé !' });
-});
+app.use('/api/stuff', stuffRoutes);
 
 module.exports = app;
