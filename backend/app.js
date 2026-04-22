b0a9e691abc42f33467b4bbb82518ae5b95965d5
Scoopote <Louis.Coulongeon2106@gmail.com>
2026-04-23 00:30:00 +0200

    feat: add user authentication with signup and login functionality

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -39,5 +40,6 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
